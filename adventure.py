39d5bb49729c35f2e5d7c33bfa1c90ed632cc63e
rishabsharma1 <151392710+rishabsharma1@users.noreply.github.com>
2024-05-05 17:11:51 -0400

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -11,13 +11,15 @@ f = open(str(sys.argv[1] ) )
 rooms = []
 try:
     raw_rooms = json.load(f)
-    if(type(raw_rooms) is not dict and 'rooms' not in raw_rooms.keys()):
-        raise Exception("Given map should be a list")
+    if(type(raw_rooms) is not dict):
+        raise Exception("Given map should be dict")
+    if('rooms' not in raw_rooms.keys() and 'start' not in raw_rooms.keys()):
+        raise Exception("Map should defenitley have a start and rooms")
     raw_rooms['rooms']=roomNamesToNumbers(raw_rooms['rooms'])
     for room in raw_rooms['rooms']:
         rooms.append(validateRoom(room, len(raw_rooms['rooms']) ) )
 except Exception as e:
-    print('Error', e)
-    exit()
+    print('Error', e,file=sys.stderr)
+    sys.exit(1)
 
 start_game(rooms, commands)
