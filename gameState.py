ef1d660c7026aea4db4a073cc2741ff7abac2bee
rishabsharma1 <151392710+rishabsharma1@users.noreply.github.com>
2024-05-05 18:31:10 -0400

    Update gameState.py

--- gameState.py
+++ gameState.py
@@ -1,29 +1,29 @@
 def start_game(rooms, commands ):
-    now = 0
+    present = 0
     inventory = []
     while(True):
-        if(rooms[now]['win'] == True ):
+        if(rooms[present]['win'] == True ):
             if('win' in inventory):
                 print('You win\nexiting.....')
                 break
             elif('lose' in inventory):
                 print('You lost\nexiting.....')
                 break
-        print(f"> {rooms[now]['name']}\n")
-        print(f"{rooms[now]['desc']}\n")
+        print(f"> {rooms[present]['name']}\n")
+        print(f"{rooms[present]['desc']}\n")
         commandsInRoom = commands[:]
-        if(len(rooms[now]['special'].keys() ) > 0):
-            for key in rooms[now]['special'].keys():
+        if(len(rooms[present]['special'].keys() ) > 0):
+            for key in rooms[present]['special'].keys():
                 commandsInRoom.append(key)
-        if(len(rooms[now]['items'] ) > 0):
-            print("Items:",", ".join(rooms[now]['items'] ) )
+        if(len(rooms[present]['items'] ) > 0):
+            print("Items:",", ".join(rooms[present]['items'] ) )
             print("")
-        if(len(rooms[now]['interactables'] ) > 0):
+        if(len(rooms[present]['interactables'] ) > 0):
             str_item = 'Interactables:'
-            for person in rooms[now]['interactables'].keys():
+            for person in rooms[present]['interactables'].keys():
                 str_item += " "+person
             print(str_item, '\n')
-        print("Exits:", " ".join(rooms[now]['exits'] ) )
+        print("Exits:", " ".join(rooms[present]['exits'] ) )
         print("")
         while(True):
             try:
@@ -40,12 +40,12 @@ def start_game(rooms, commands ):
                     if(len(command) != 2):
                         print("Sorry, you need to 'go' somewhere.")
                         continue
-                    if(command[1] not in rooms[now]['exits'].keys() ):
+                    if(command[1] not in rooms[present]['exits'].keys() ):
                         print(f"There's no way to go {command[1]}.")
                         continue
                     else:
                         print(f"You go {command[1]}.\n")
-                        present = rooms[now]['exits'][command[1]]
+                        present = rooms[present]['exits'][command[1]]
                         break
                 elif(command[0] == 'look'):
                     break
@@ -64,13 +64,13 @@ def start_game(rooms, commands ):
                     if(len(command) > 2):
                         for i in range(2, len(command) ):
                             command[1] += (" "+command[i])
-                    if(command[1] not in rooms[now]['items'] ):
+                    if(command[1] not in rooms[present]['items'] ):
                         print(f"There's no {command[1]} anywhere.")
                         continue
                     else:
                         print(f"You pick up the {command[1]}.")
                         inventory.insert(0,command[1] )
-                        rooms[now]['items'].remove(command[1] )
+                        rooms[present]['items'].remove(command[1] )
                         continue
                 elif(command[0] == 'drop'):
                     if(len(command) < 2):
@@ -85,23 +85,23 @@ def start_game(rooms, commands ):
                     else:
                         print("You droped  " + command[1] + " here")
                         inventory.remove(command[1] )
-                        rooms[now]['items'].append(command[1] )
+                        rooms[present]['items'].append(command[1] )
                         continue
                 elif(command[0] == 'talk'):
                     if(len(command) != 2 ):
                         print("Sorry, you need to 'talk' something.")
                         continue
-                    if(command[1] not in rooms[now]['interactables'].keys() ):
+                    if(command[1] not in rooms[present]['interactables'].keys() ):
                         print("There's no "+command[1]+" anywhere")
                         continue
                     else:
-                        print(command[1]+" : "+rooms[now]['interactables'][command[1]]['dailogue'])
+                        print(command[1]+" : "+rooms[present]['interactables'][command[1]]['dailogue'])
                         continue
                 elif(command[0] == 'give'):
                     if(len(command) != 3):
                         print("Sorry,you need to 'give' something to someone")
                         continue
-                    if(command[1] not in rooms[now]['interactables'].keys() ):
+                    if(command[1] not in rooms[present]['interactables'].keys() ):
                         print("There's no "+command[1]+" anywhere")
                         continue
                     else:
@@ -113,11 +113,11 @@ def start_game(rooms, commands ):
                             continue
                         else:
                             inventory.remove(command[2] )
-                            inventory.append(rooms[now]['interactables'][command[1] ]['give'][command[2] ] )
-                            print("By giving "+command[2]+" to "+command[1]+", you got "+rooms[now]['interactables'][command[1]]['give'][command[2]])
+                            inventory.append(rooms[present]['interactables'][command[1] ]['give'][command[2] ] )
+                            print("By giving "+command[2]+" to "+command[1]+", you got "+rooms[present]['interactables'][command[1]]['give'][command[2]])
                             continue
-                elif(command[0] in rooms[now]['special'].keys() ):
-                    checklist = rooms[now]['special'][command[0] ]['checklist']
+                elif(command[0] in rooms[present]['special'].keys() ):
+                    checklist = rooms[present]['special'][command[0] ]['checklist']
                     checkFlag = True
                     for thing in checklist:
                         if(thing not in inventory):
@@ -126,14 +126,14 @@ def start_game(rooms, commands ):
                             break
                     
                     if(checkFlag):
-                        inventory.append(rooms[now]['special'][command[0] ]['success'] )
+                        inventory.append(rooms[present]['special'][command[0] ]['success'] )
                         print(command[0]+" successful\n")
                         for thing in checklist:
                             if(thing in inventory):
                                 inventory.remove(thing)
                         break
-                    elif('failure' in rooms[now]['special'][command[0] ].keys() ):
-                        inventory.append(rooms[now]['special'][command[0] ]['failure'] )
+                    elif('failure' in rooms[present]['special'][command[0] ].keys() ):
+                        inventory.append(rooms[present]['special'][command[0] ]['failure'] )
                         break
                     continue
             else:
